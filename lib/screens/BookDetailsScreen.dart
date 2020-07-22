249131a409d055d10848f68bf4b90d94ee37aa19
Matheus Brandino <m.v.brandino@gmail.com>
2020-07-21 22:55:36 -0300

    create collapse toolbar

--- lib/screens/BookDetailsScreen.dart
+++ lib/screens/BookDetailsScreen.dart
@@ -1,5 +1,6 @@
 import 'package:casa_do_codigo/models/Book.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BookDetailsScreen extends StatelessWidget {
   @override
@@ -7,9 +8,52 @@ class BookDetailsScreen extends StatelessWidget {
     final Book book = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(book.name),
+        body: NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            expandedHeight: 350.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                book.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+              background: Stack(
+                fit: StackFit.passthrough,
+                children: <Widget>[
+                  Image.network(
+                    book.image,
+                    fit: BoxFit.fill,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.center,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withAlpha(0),
+                          Colors.black12,
+                          Colors.black54
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ];
+      },
+      body: Center(
+        child: Text("Sample Text"),
       ),
-    );
+    ));
   }
 }
