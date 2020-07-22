bdf5dc6b98fdfb24831aa30f4bef52ff478fd630
Matheus Brandino <m.v.brandino@gmail.com>
2020-07-21 23:28:27 -0300

    refactor to use a map instead forEach
    Co-authored-by: Yuri <yurimatheusb@hotmail.com>

--- lib/data/api/BookConverter.dart
+++ lib/data/api/BookConverter.dart
@@ -6,12 +6,9 @@ import 'BooksResponse.dart';
 
 class BookConverter {
   static List<Book> decode(String response) {
-    List<Book> books = List();
     var responseBook = BooksResponse.fromJson(json.decode(response));
-
-    responseBook.booksToConvert.forEach((element) {
-      books.add(Book.fromJson(element));
-    });
-    return books;
+    return responseBook.booksToConvert
+        .map((element) => Book.fromJson(element))
+        .toList();
   }
 }
